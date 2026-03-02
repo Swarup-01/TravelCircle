ff6dd1c85b238461de237a31a64f4874c97cba94
swarup gonte <swarupgonte@gmail.com>
2026-03-03 02:16:04 +0530

    change routes

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
-app.use("/trips", tripRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/trips", tripRoutes);
 
 app.get("/", (req, res) => {
   res.send("TravelCircle API is running...");
