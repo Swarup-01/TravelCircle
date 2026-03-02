f4f730b3d1ed7506f36f6d6d93a943024bdc0034
swarup gonte <swarupgonte@gmail.com>
2026-03-02 11:25:20 +0530

    connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://travel-circle.vercel.app/api",
 });
 
 // Automatically attach token if exists
